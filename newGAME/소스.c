f5c3c315eab344a4484a65e19bfe9fc46848fabe
Lee sang hoon <ttjjdd9340@naver.com>
2024-12-04 21:11:28 +0900

    보드를 출력하는 함수

--- newGAME/소스.c
+++ newGAME/소스.c
@@ -10,3 +10,16 @@ char board[3][3] = {
 
 // 현재 플레이어를 'X'로 설정
 char currentPlayer = 'X';
+// 보드를 출력하는 함수
+void printBoard() {
+    printf("\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf(" %c ", board[i][j]);
+            if (j < 2) printf("|");
+        }
+        printf("\n");       
+        if (i < 2) printf("---|---|---\n");
+    }
+    printf("\n");
+}
