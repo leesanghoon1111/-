e65df7f0526ec994d929e59717af7cbfded0fae7
Lee sang hoon <ttjjdd9340@naver.com>
2024-12-04 21:06:58 +0900

    게임보드 배열 초기화 및 현재 플레이어 x설정

--- newGAME/소스.c
+++ newGAME/소스.c
@@ -1,3 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+// 게임 보드 배열 초기화
+char board[3][3] = {
+    {'1', '2', '3'},
+    {'4', '5', '6'},
+    {'7', '8', '9'}
+};
+
+// 현재 플레이어를 'X'로 설정
+char currentPlayer = 'X';
